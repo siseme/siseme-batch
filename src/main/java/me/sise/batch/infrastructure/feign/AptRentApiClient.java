95361ecc1fd7bc42b4c287aa331acf66879e9825
ih.lee <bamplee@gmail.com>
2019-07-06 10:21:12 +0900

    전월세 API 매핑값 설정

--- src/main/java/me/sise/batch/infrastructure/feign/AptRentApiClient.java
+++ src/main/java/me/sise/batch/infrastructure/feign/AptRentApiClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "rent-list-api")
 public interface AptRentApiClient {
-    @GetMapping
+    @GetMapping("/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptRent")
     AptRentDetail getAptRentDetailList(@RequestParam("ServiceKey") String serviceKey,
                                        @RequestParam("DEAL_YMD") String dealYmd,
                                        @RequestParam("LAWD_CD") String lawdCd,
