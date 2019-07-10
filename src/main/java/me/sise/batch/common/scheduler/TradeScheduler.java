ee9f798d19fef1ac8c2acaf521613402056d7d5c
ih.lee <bamplee@gmail.com>
2019-07-10 10:37:11 +0900

    차트 배치 10년전부터 싱크하도록 수정

--- src/main/java/me/sise/batch/common/scheduler/TradeScheduler.java
+++ src/main/java/me/sise/batch/common/scheduler/TradeScheduler.java
@@ -27,30 +27,30 @@ public class TradeScheduler {
 
     @Scheduled(fixedDelay = Integer.MAX_VALUE)
     public void sync() {
-        YearMonth currentYearMonth = YearMonth.now();
+        YearMonth currentYearMonth = YearMonth.now().minusMonths(120);
         for (int i = 0; i < 120; i++) {
+/*
             tradeSyncService.syncOpenApiList(currentYearMonth);
             tradeSyncService.syncDataList(currentYearMonth);
-/*
+*/
             tradeSyncService.syncTradeStatsList(currentYearMonth, RegionType.SIDO);
             tradeSyncService.syncTradeStatsList(currentYearMonth, RegionType.GUNGU);
             tradeSyncService.syncTradeStatsList(currentYearMonth, RegionType.DONG);
-*/
+/*
             ticketSyncService.syncOpenApiList(currentYearMonth);
             ticketSyncService.syncDataList(currentYearMonth);
-/*
+*/
             ticketSyncService.syncTradeStatsList(currentYearMonth, RegionType.SIDO);
             ticketSyncService.syncTradeStatsList(currentYearMonth, RegionType.GUNGU);
             ticketSyncService.syncTradeStatsList(currentYearMonth, RegionType.DONG);
-*/
+/*
             rentSyncService.syncOpenApiList(currentYearMonth);
             rentSyncService.syncDataList(currentYearMonth);
-/*
+*/
             rentSyncService.syncTradeStatsList(currentYearMonth, RegionType.SIDO);
             rentSyncService.syncTradeStatsList(currentYearMonth, RegionType.GUNGU);
             rentSyncService.syncTradeStatsList(currentYearMonth, RegionType.DONG);
-*/
-            currentYearMonth = currentYearMonth.minusMonths(1);
+            currentYearMonth = currentYearMonth.plusMonths(1);
         }
     }
 }
